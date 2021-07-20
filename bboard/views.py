4c2e33d996fdb8ec3ea539270db1768d5feed33d
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-07-20 21:36:36 +0500

    Update board/views.py
    - изменил используемый метод на сокращённый

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Bb
 
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs': bbs})
