c405a2a3270c749f6f81bf3d820162c26dec4ab2
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-07-20 21:29:50 +0500

    Init project
    - Инициализация проекта на django
    - Добавлено приложение
    - Обновлена бд для сайта
    - Добавлен html шаблон сайта

---
+++ bboard/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
+
+
+def index(request):
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
