8d4faf218c1880753bfb54b213617d4197ac24f0
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-07-22 00:05:27 +0500

    Update models
    - Обновил модели
    - Добваил модель "Рубрики", являющейся первичным кллючом для модели "Объявления"

--- bboard/models.py
+++ bboard/models.py
@@ -7,7 +7,18 @@ class Bb(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
 
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
