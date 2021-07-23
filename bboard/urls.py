7873b7b075334bac2a3626f35e555923b640adf5
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-07-24 00:19:15 +0500

    Add new page
    - добавлена новая страница с создданием нового объявления
    - ревлизована, через контройлер класс в vewes.py

--- bboard/urls.py
+++ bboard/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.urls import path
 
-from bboard.views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .forms import BbForm
 
 from .models import Bb, Rubric
 
@@ -16,3 +20,14 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
