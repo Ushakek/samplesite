f44c7d5739203fd1592fc4961a723d46126dfb22
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-07-23 23:02:45 +0500

    Update model
    - Обновил модель, добавил рубрики
    - Разбил данные по рубрикам
    - Вывевел отображение рубрик

--- bboard/models.py
+++ bboard/models.py
@@ -18,6 +18,9 @@ class Bb(models.Model):
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
--- bboard/urls.py
+++ bboard/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 
-from bboard.views import index
+from bboard.views import index, by_rubric
 
 urlpatterns = [
+    path('<int:rubric_id>/', by_rubric),
     path('', index),
 ]
