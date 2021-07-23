160cf262e1b0776c8ae0c4531676395b1d0a501c
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-07-23 23:28:32 +0500

    Update urls
    - Обновил url запросы на обратное разрешение

--- bboard/urls.py
+++ bboard/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from bboard.views import index, by_rubric
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 ]
